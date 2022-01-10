36fb5ed979cb8696448f06b2988d01c8f8c75eea
Lucas Coelho <lucascoelhodacosta@gmail.com>
2022-01-10 16:39:31 -0300

    chore: implement thread pool

--- book/web-server/src/bin/main.rs
+++ book/web-server/src/bin/main.rs
@@ -1,3 +1,6 @@
+use web_server::ThreadPool;
+use std::thread;
+use std::time::Duration;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -5,11 +8,14 @@ use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -18,15 +24,18 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
     let contents = fs::read_to_string(filename).unwrap();
-
 
     let response = format!(
         "{}\r\nContent-Length: {}\r\n\r\n{}",
